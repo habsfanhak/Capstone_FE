7b5b48ed29ad4a9fc777434181fe034649db99dd
LwinMateo <lymateo-lopez@myseneca.ca>
2024-03-18 14:42:53 -0400

    view bike products FE

--- pages/bikes.js
+++ pages/bikes.js
@@ -1,6 +1,7 @@
-import { Button, Card, ListGroup } from 'react-bootstrap';
+import { Button, Card, ListGroup, Container, Row, Col } from 'react-bootstrap';
 import { getBikes } from "@/lib/userActions";
 import { useState, useEffect } from "react";
+import bike_styles from '../styles/Bikes.module.css'
 
 export default function Bikes() {
     const [bikes, setBikes] = useState([]);
@@ -18,27 +19,38 @@ export default function Bikes() {
             <br/>
             <center><h2>Bikes</h2></center>
             <br/>
-            <div>
-                {bikes.map((bike) => {
-                    return (
-                        <Card key={bike._id}>
-                            <Card.Body>
-                                <Card.Title>{bike.brand}</Card.Title>
-                                <Card.Text>
-                                    {bike.model}
-                                </Card.Text>
-                            </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroup.Item>Price: ${bike.price}</ListGroup.Item>
-                            </ListGroup>
-                            <Card.Body>
-                                <Button variant="primary">Add to Cart</Button>
-                            </Card.Body>
-                        </Card>
-                    )
-                })}
-                
-            </div>
+            <Container>
+                <Row>
+                    {bikes.map((bike) => {
+                        return (
+                            <Col sm={12} md={4} key={bike._id}>
+                                <Card className={bike_styles.custom_card}>
+                                    <Card.Body>
+                                        <Card.Title>{bike.brand}</Card.Title>
+                                        <Card.Text>
+                                            {bike.model}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <ListGroup className="list-group-flush">
+                                        <ListGroup.Item>Type: {bike.type}</ListGroup.Item>
+                                        <ListGroup.Item>Frame: {bike.frame_material}</ListGroup.Item>
+                                        
+                                        <ListGroup.Item>Wheel Size: {bike.wheelSize}</ListGroup.Item>
+                                        <ListGroup.Item>Suspension: {bike.suspension_type}</ListGroup.Item>
+                                        <ListGroup.Item>Gear Type: {bike.gear_type}</ListGroup.Item>
+
+                                        <ListGroup.Item>Price: ${bike.price}</ListGroup.Item>
+                                        <ListGroup.Item>Available: {bike.available_quantity || 'Not Available'}</ListGroup.Item>
+                                    </ListGroup>
+                                    <Card.Body>
+                                        <Button variant="primary">Add to Cart</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Container>
         </>
     )
 }
