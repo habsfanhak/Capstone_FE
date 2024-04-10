db12c5182e48fdb8c99dad6c3e978e95b3bae639
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2024-04-10 00:30:42 -0400

    Added individual bike view and payment

---
+++ pages/paymentSuccess.js
@@ -0,0 +1,33 @@
+import { Button, Container } from "react-bootstrap"
+import { useRouter } from "next/router";
+import { updateQuantity } from "@/lib/userActions";
+import { useEffect } from "react";
+
+export default function PaymentSuccess(){
+    const router = useRouter();
+    const model = router.query.model;
+
+    useEffect(() => {
+        async function updateQty() {
+            updateQuantity(model);
+        }
+        updateQty();
+    }, [model]);
+
+    function returnHome(e){
+        e.preventDefault();
+        router.push("/");
+    }
+
+    return(
+        <>  
+            <Container>
+                <br/><br/><br/><br/>
+                <h1>Your Order Has Been Placed</h1>
+                <p>Thank you for purchasing</p>
+                <br /><br />
+                <Button variant="primary" onClick={returnHome}>Return Home</Button>
+            </Container>
+        </>
+    )
+}
